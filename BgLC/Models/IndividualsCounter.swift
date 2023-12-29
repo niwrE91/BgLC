206d9c99709d11ecfdfd0b9a781f2efef9787018
Warkentin Erwin <warkentinerwin@icloud.com>
2023-12-29 14:23:45 +0100

    added first layout

---
+++ BgLC/Models/IndividualsCounter.swift
@@ -0,0 +1,15 @@
+//
+//  IndividualsCounter.swift
+//  BgLC
+//
+//  Created by Erwin Warkentin on 29.12.23.
+//
+
+import Foundation
+
+// MARK: - IndividualsCounter
+struct IndividualsCounter {
+    let name: String
+    let count: Int
+    let image: String?
+}
---
+++ BgLC/Views/MainView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  BgLC
+//
+//  Created by Erwin Warkentin on 29.12.23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        
+        VStack(spacing: 150) {
+            HStack{
+                VStack{
+                    Text("Spieler")
+                    HStack{
+                        Button(action: {
+                                  print("button pressed")
+
+                                }) {
+                                    Image(systemName: "minus.circle.fill")
+                                    .renderingMode(.original)
+                                }
+                        Text("3")
+                        Button(action: {
+                                  print("button pressed")
+
+                                }) {
+                                    Image(systemName: "plus.circle.fill")
+                                    .renderingMode(.original)
+                                }
+                    }
+                }
+                Image(systemName: "person.2")
+            }.background(
+                Image(systemName: "battery.0")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 300)
+            )
+            HStack{
+                VStack{
+                    Text("Gegner")
+                    HStack{
+                        Button(action: {
+                                  print("button pressed")
+
+                                }) {
+                                    Image(systemName: "minus.circle.fill")
+                                    .renderingMode(.original)
+                                }
+                        Text("3")
+                        Button(action: {
+                                  print("button pressed")
+
+                                }) {
+                                    Image(systemName: "plus.circle.fill")
+                                    .renderingMode(.original)
+                                }
+                    }
+                }
+                Image(systemName: "person.icloud")
+            }
+            .background(
+                Image(systemName: "battery.0")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 300)
+            )
+        }
+        .padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
