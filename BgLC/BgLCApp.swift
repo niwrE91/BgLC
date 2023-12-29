fb6b98b3780ac9085c36f93c02d5942fbcdf880b
Warkentin Erwin <warkentinerwin@icloud.com>
2023-12-29 13:07:14 +0100

    Init Project

---
+++ BgLC/BgLCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BgLCApp.swift
+//  BgLC
+//
+//  Created by Erwin Warkentin on 29.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BgLCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
